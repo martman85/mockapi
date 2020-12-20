db9996642e322fc12ca829021c99a8886a73a363
Mart Oosterveld <info@martoosterveld.nl>
2020-12-20 08:07:04 +0100

    added functionality for user detail

--- server/index.js
+++ server/index.js
@@ -10,11 +10,15 @@ let users = require('./data/users.json');
 app.get('/users', function (req, res) {
   res.json(users);
 })
+app.get('/users/:id', function (req, res) {
+  res.json(users.filter(user => user.id == req.params.id)[0]);
+  return;
+})
 
 var dir = path.join(__dirname, 'images');
 
 app.use(express.static(dir));
 
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log('Listening on http://localhost:3000/');
 });
